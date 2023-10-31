fc6135ea1aa8865bed42231c35adbfb4b5787533
Ali-Fikri <alifikripr@gmail.com>
2023-10-31 05:55:49 +0200

    create navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { AiFillBug } from 'react-icons/ai';
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issue" },
+  ];
+
+  return (
+    <nav className="flex items-center gap-6 border-b p-6">
+      <Link href="/"><AiFillBug /></Link>
+      <ul className="flex gap-6">
+        {links.map((link) => (
+          <Link className="text-zinc-500 hover:text-zinc-800" key={link.href} href={link.href}>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
