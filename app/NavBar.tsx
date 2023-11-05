9b064675e9377064c99df19188d2860b4daba4c2
Ali-Fikri <alifikripr@gmail.com>
2023-11-05 19:54:24 +0200

    setup prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { AiFillBug } from 'react-icons/ai';
+import { AiFillBug } from "react-icons/ai";
 const NavBar = () => {
   const pathname = usePathname();
 
@@ -13,10 +13,18 @@ const NavBar = () => {
 
   return (
     <nav className="flex items-center gap-6 border-b p-6">
-      <Link href="/"><AiFillBug /></Link>
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex gap-6">
         {links.map((link) => (
-          <Link className={`${link.href === pathname ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800`} key={link.href} href={link.href}>
+          <Link
+            className={`${
+              link.href === pathname ? "text-zinc-900" : "text-zinc-500"
+            } hover:text-zinc-800`}
+            key={link.href}
+            href={link.href}
+          >
             {link.label}
           </Link>
         ))}
